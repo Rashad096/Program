c5bdcc15d75ed674aa017c3b0b7dc2e0214f20f4
Rashad096 <abbasovrashad1998@gmail.com>
2022-12-12 15:02:58 +0400

    new project

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task No1
+            //-Verilmiş M dəyərindən verilmiş N dəyərinədək 21 - ə bölünən ədədlərin ədədi ortasını tapan proqram
+
+            //  Console.WriteLine("M ededini daxil et:");
+            //  var input1 = Console.ReadLine();
+            //  var M = Convert.ToInt32(input1);
+
+            //  Console.WriteLine("N ededini daxil et:");
+            //  var input2 = Console.ReadLine();
+            //  var N = Convert.ToInt32(input2);
+
+
+
+            // double sum = 0;
+            //double count = 0;
+
+
+
+            //  while (M < N)
+            //  {
+            //      int var = M;
+            //      if (var % 21 == 0)
+            //      {
+            //          sum += M;
+            //          count++;
+            //      }
+            //      M++;
+            //  }
+            //  Console.WriteLine(sum / count);
+
+
+
+            //Task No2
+            //Verilmiş ədədin rəqəmləri cəmini tapan proqram
+
+
+            //Console.WriteLine("Ededi daxil edin:");
+            // var num = Console.ReadLine();
+            // int a = Convert.ToInt32(num);
+            // int sum = 0;
+            // int lastnum= 0;
+
+            // while (a!=0)
+            // {
+            //     lastnum = a % 10;
+            //     sum += lastnum;
+            //     int c = (a - lastnum) / 10;
+            //     a = c;
+            // }
+            // Console.WriteLine(sum);
+
+            //task No3
+            //Verilmiş ayın ədədinə görə hansı fəsil olduğunu tapan proqram
+
+            //Console.WriteLine("Heftenin gununu daxil edin:");
+            //var input = Console.ReadLine();
+            //int dayoftheweek = Convert.ToInt32(input);
+
+            //    if (dayoftheweek == 1)
+            //    {
+            //        Console.WriteLine("Monday");
+
+            //    }
+            //    else if (dayoftheweek == 2)
+            //    {
+            //        Console.WriteLine("Tuesday");
+
+            //    }
+            //    else if (dayoftheweek == 3)
+            //    {
+            //        Console.WriteLine("Wednesday");
+
+            //    }
+            //    else if (dayoftheweek == 4)
+            //    {
+            //        Console.WriteLine("Thursday");
+            //    }
+            //    else if (dayoftheweek == 5)
+            //    {
+            //        Console.WriteLine("Friday");
+
+            //    }
+            //    else if (dayoftheweek == 6 || dayoftheweek == 7)
+            //    {
+            //        Console.WriteLine("WeekEnd");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("reqem sehfdir");
+
+            //    }
+
+
+
+            //Task No5
+            // Verilmiş 2 ədəd üzərində verilmiş operator simvoluna uyğun riyazi əməliyyat aparan proqram. 
+            //Misalçün consoledan 5,10 və + daxil edilsə 5 və 10-u toplayıb nəticəsini göstərsin.
+            //Console-dan daxil edilən operator + , -, *, / operatorlarından biri deyilsə operator yanlışdır yazılsın.
+
+            //Console.WriteLine("ilk ededi daxil et:");
+            //var input1 = Console.ReadLine();
+            //double a = Convert.ToDouble(input1);
+
+            //Console.WriteLine("Ikinci ededi daxil et:");
+            //var input2 = Console.ReadLine();
+            //double b = Convert.ToDouble(input2);
+
+            //Console.WriteLine("Gorulecek riyazi emeliyyati daxil et:");
+            //var op = Console.ReadLine();
+
+            //if (op =="+")
+            //{
+            //    Console.WriteLine(a+b);
+            //}
+            //else if (op=="-")
+            //{
+            //    Console.WriteLine(a-b);
+            //}
+            //else if (op=="*")
+            //{
+            //    Console.WriteLine(a*b);
+            //}
+            //else if (op=="/")
+            //{
+            //    Console.WriteLine(a / b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Emeliyyat yanlishdir");
+            //}
+
+            //Task No4
+            //Verilmiş yazıda rəqəm olmamasını yoxlayan proqram,
+            // əgər rəqəm varsa yenidən yazının daxil edilməsini istəməlidir ta ki rəqəmsiz daxil edilənədək.
+
+            Console.WriteLine("Yazini daxil et:");
+            var word = Console.ReadLine();
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < word.Length; i++)
+            {
+               
+                
+            }
+
+           
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
